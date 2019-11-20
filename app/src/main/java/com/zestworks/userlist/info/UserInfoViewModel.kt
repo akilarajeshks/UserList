0bfd60249f6778218adc11e4615c41a17b13e6de
Akila <akilarajesh20@gmail.com>
2019-11-20 22:26:23 +0000

    Initial commit

---
+++ app/src/main/java/com/zestworks/userlist/info/UserInfoViewModel.kt
@@ -0,0 +1,7 @@
+package com.zestworks.userlist.info
+
+import androidx.lifecycle.ViewModel
+
+class UserInfoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/zestworks/userlist/listing/UserListViewModel.kt
@@ -0,0 +1,7 @@
+package com.zestworks.userlist.listing
+
+import androidx.lifecycle.ViewModel
+
+class UserListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
